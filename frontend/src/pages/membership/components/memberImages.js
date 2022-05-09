d453318645d4ca66dae8d0a8764cff305066d0ea
Niall1001 <niallwalters135@gmail.com>
2022-05-09 20:20:38 +0100

    membership page continued

--- frontend/src/pages/membership/components/memberImages.js
+++ frontend/src/pages/membership/components/memberImages.js
@@ -3,6 +3,12 @@ import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import { Grid } from "@mui/material";
 import eastDown from "../../../images/east-down.jpg";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
 const MemberImages = () => {
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -15,37 +21,91 @@ const MemberImages = () => {
   return (
     <div style={{ display: "flex" }}>
       <Grid container spacing={5}>
-        <Grid item xs={12} md={12}>
-        <Item xs={12} md={12} style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-around",
-            columnCount: 4,
-            breakInside: "avoid-column"
-          }}>
-        <img
-          src={eastDown}
-          loading="lazy"
-          style={{ breakInside: "avoid-column", height: "323px", width: "269px" }}
+        <Grid item xs={12} md={4}>
+  <Item xs={12} md={4} style={{
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    columnCount: 4,
+    breakInside: "avoid-column"
+  }}>
+  <Card sx={{ maxWidth: 345, margin: "10px"}}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={eastDown}
+          alt="green iguana"
         />
-        <img
-          src={eastDown}
-          loading="lazy"
-          style={{ breakInside: "avoid-column", height: "323px", width: "269px" }}
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Family
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Members who are parents or guardians with children under the age of 18 or in University
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </Item>
+    </Grid>
+    <Grid item xs={12} md={4}>
+  <Item xs={12} md={4} style={{
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    columnCount: 4,
+    breakInside: "avoid-column"
+  }}>
+    <Card sx={{ maxWidth: 345, margin: "10px" }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={eastDown}
+          alt="green iguana"
         />
-    <img
-      src={eastDown}
-      loading="lazy"
-      style={{ breakInside: "avoid-column", height: "323px", width: "269px" }}
-    />
-    <img
-      src={eastDown}
-      loading="lazy"
-      style={{ breakInside: "avoid-column", height: "323px", width: "269px" }}
-    />
-  </Item>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Single
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Members who are 18 years of age and over, with discounted rates for those under 30 years old
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </Item>
+    </Grid>
+    <Grid item xs={12} md={4}>
+  <Item xs={12} md={4} style={{
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    columnCount: 4,
+    breakInside: "avoid-column"
+  }}>
+    <Card sx={{ maxWidth: 345, margin: "10px" }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={eastDown}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Temp
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Temp Description
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </Item>
+    </Grid>
         </Grid>
-      </Grid>
     </div>
   );
 };
